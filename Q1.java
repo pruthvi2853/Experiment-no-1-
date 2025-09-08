9d8fa0d79aae8df76533ba13c42cf0d97cb6f0cc
Pruthviraj Patil <patilpruthviraj901@gmail.com>
2025-09-08 16:53:13 +0530

    Update hello.java

--- Q1.java
+++ Q1.java
@@ -1,34 +1,9 @@
 import java.util.Scanner;
-public class Q1 {
+public class Hello {
     public static void main(String[] args) {
-        String[] ones = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String[] teens = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-                          "sixteen", "seventeen", "eighteen", "nineteen"};
-        String[] tens = {"", "", "twenty", "thirty", "forty", "fifty",
-                         "sixty", "seventy", "eighty", "ninety"};
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number 1 to 100: ");
-        int num = sc.nextInt();
-
-        if (num == 100) {
-            System.out.println("one hundred");
-        } else if (num >= 1 && num < 10) {
-            System.out.println(ones[num - 1]);
-        } else if (num >= 10 && num < 20) {
-            System.out.println(teens[num - 10]);
-        } else if (num >= 20 && num < 100) {
-            int tenPart = num / 10;
-            int onePart = num % 10;
-            String result = tens[tenPart];
-            if (onePart != 0) {
-                result += " " + ones[onePart - 1];
-            }
-            System.out.println(result);
-        } else {
-            System.out.println("Invalid input");
-        }
-
-        sc.close();
+        
+        System.out.print("Hello world ");
+        
     }
 }
+

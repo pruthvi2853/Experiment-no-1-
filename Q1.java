f4493c5b7f592e1f1e56b949b5b006dded5419d7
Pruthviraj Patil <patilpruthviraj901@gmail.com>
2025-09-08 16:53:47 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-public class Hello {
+public class Q1{
     public static void main(String[] args) {
         
         System.out.print("Hello world ");
         
     }
 }
+
 
